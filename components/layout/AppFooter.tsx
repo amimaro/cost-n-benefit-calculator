6a260a1b77713849c939beded136c07e80b8d060
amimaro <amir.zahlan@gmail.com>
2022-10-16 16:23:48 -0300

    Create base layout

---
+++ components/layout/AppFooter.tsx
@@ -0,0 +1,7 @@
+export default function AppFooter() {
+  return (
+    <div className="sticky top-[100vh]">
+      <div className="flex justify-center py-1"></div>
+    </div>
+  )
+}
---
+++ components/layout/AppHeader.tsx
@@ -0,0 +1,7 @@
+export default function AppHeader() {
+  return (
+    <div className="text-2xl font-bold bg-slate-700 text-white px-2 py-4 mb-2 shadow-sm">
+      Cost and benefit calculator
+    </div>
+  )
+}
