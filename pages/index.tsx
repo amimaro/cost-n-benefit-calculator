237b6966f7e664171a8b0899c3a83853cf57970c
amimaro <amir.zahlan@gmail.com>
2022-10-17 20:35:09 -0300

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { loadComparisons } from '../services/lowdb'
 
 const Home: NextPage = () => {
+  const [comparisons, setComparisons] = useState([])
+
+  useEffect(() => {
+    setComparisons(loadComparisons())
+  }, [])
+
   return <div className="w-full h-full"></div>
 }
 
