a205092929ad70a5888928a1d830fcc43ebe0b61
João Lucas Costa <61983891+JoaoGennaro@users.noreply.github.com>
2022-10-20 20:04:30 -0300

    Add Head title to project (#9)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import AppLayout from '../components/layout/AppLayout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>
+      <Head>
+        <title>Cost and benefit calculator</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </AppLayout>
   )
